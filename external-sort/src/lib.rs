ee50de0d65341f7e805e05db6e3077044062c964
Samrat Man Singh <samratmansingh@gmail.com>
2017-11-12 10:54:30 +0545

    Write to file once buffer is full

--- external-sort/src/lib.rs
+++ external-sort/src/lib.rs
@@ -88,7 +88,7 @@ fn compute_offsets(row_num: usize, valsize: usize) -> RowOffsets {
 }
 
 
-fn read_record(offsets: RowOffsets, data: Vec<u8>, row_num: usize)
+fn read_record(offsets: RowOffsets, data: Vec<u8>)
                -> (i32,Vec<u8>) {
     let key = bytearray_to_i32(
         data[offsets.key_offset..offsets.val_offset].to_vec());
@@ -100,6 +100,22 @@ pub fn mem_move(dest: &mut [u8], src: &[u8]) {
     for (d, s) in dest.iter_mut().zip(src) {
         *d = *s
     }
+}
+
+fn write_record(offsets: RowOffsets, data: &mut [u8],
+                key: i32, val: &[u8]) {
+    mem_move(&mut data[offsets.key_offset..offsets.val_offset], &i32_to_bytearray(key));
+    mem_move(&mut data[offsets.val_offset..offsets.row_end], val);
+}
+
+fn serialize_records(records: Vec<(i32, Vec<u8>)>) -> [u8; PAGE_SIZE] {
+    let mut storage = [0; PAGE_SIZE];
+    let valsize = 4;
+    for (i, (k,v)) in records.into_iter().enumerate() {
+        let offsets = compute_offsets(i, valsize);
+        write_record(offsets, &mut storage, k, &v);
+    }
+    storage
 }
 
 pub struct BufferPoolManager {
@@ -135,6 +151,14 @@ impl BufferPoolManager {
         }
     }
 
+    pub fn write_page(mut file: &File, page_id: usize, data: &[u8]) {
+        let offset = (page_id * PAGE_SIZE) as u64;
+        file.seek(SeekFrom::Start(offset))
+            .expect("Could not seek to offset");
+        file.write(data).expect("write failed");
+        file.flush().expect("flush failed");
+    }
+
     pub fn read_records(&self, data: &[u8]) -> Vec<(i32,Vec<u8>)> {
         let num_records_bytes = data[0..8].to_vec();
         let num_records = bytearray_to_usize(num_records_bytes);
@@ -143,7 +167,7 @@ impl BufferPoolManager {
         let valsize = 4;        // NOTE: fixed size(hacky)
         for i in 0..num_records {
             let offsets = compute_offsets(i, valsize);
-            records.push(read_record(offsets, data.to_vec(), i));
+            records.push(read_record(offsets, data.to_vec()));
         }
 
         records
@@ -177,6 +201,9 @@ impl BufferPoolManager {
             .iter().peekable();
         let mut first_a = a_iter.next();
         let mut first_b = b_iter.next();
+        
+        let output_buffer = [0; PAGE_SIZE];
+        let mut output_page_id = 0;
         loop {
             match (first_a, first_b) {
                 (None, None) => break,
@@ -184,6 +211,7 @@ impl BufferPoolManager {
                     if fa.0 < fb.0 {
                         self.output_buffer.records.push(fa.clone());
                         first_a = a_iter.next();
+                        
                     } else {
                         self.output_buffer.records.push(fb.clone());
                         first_b = b_iter.next();
@@ -198,14 +226,20 @@ impl BufferPoolManager {
                     self.output_buffer.records.push(fb.clone());
                     first_b = b_iter.next();
                 },
-            }
+            };
 
+            // check if output buffer is full(# records > max). If it
+            // is full, write to file. And continue with empty buffer.
             if self.output_buffer.records.len() >= self.records_per_page {
-                println!("TODO: write to page");
+                let records = mem::replace(&mut self.output_buffer.records,
+                                           vec![]);
+                let new_page_data =
+                    serialize_records(records);
+                Self::write_page(&self.files[output_file_index],
+                                 output_page_id,
+                                 &new_page_data);
+                output_page_id += 1;
             }
-            // TODO: check if output buffer is full(# records >
-            // max). If it is full, write to file. And continue with
-            // empty buffer.
         }
 
         println!("{:?}", self.output_buffer.records);
